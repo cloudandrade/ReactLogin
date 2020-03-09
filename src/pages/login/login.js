22b4fe28beba376e8bcec8e63ae5197b820d299e
Jan <jan.andrade@capgemini.com>
2020-03-09 17:08:45 -0300

    modificando textfield

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,16 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react';
 import { Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import './styles.css';
 
-export default function login(props) {
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	const [isRegister, setIsRegister] = useState(false);
+const useStyles = makeStyles({
+	padding: '15px',
+	boxSizing: 'border-box',
+	underline: {
+		'&&&:before': {
+			borderBottom: 'none',
+		},
+		'&&:after': {
+			borderBottom: 'none',
+		},
+	},
+});
 
+export default function login(props) {
+	const [isRegister, setIsRegister] = useState(false);
+	const classes = useStyles();
 	return (
 		<div className="login-page">
 			<div className="form">
 				<form className="register-form">
-					<TextField label="name" variant="outlined" />
+					<TextField
+						label="name"
+						variant="outlined"
+						InputProps={{ classes }}
+					/>
 					<input type="password" placeholder="password" />
 					<input type="text" placeholder="email address" />
 					<Button>create</Button>
@@ -20,10 +38,16 @@ export default function login(props) {
 				</form>
 				<form className="login-form">
 					<input type="text" placeholder="username" />
+					<TextField
+						label="name"
+						variant="filled"
+						InputProps={{ classes }}
+					/>
 					<input type="password" placeholder="password" />
 					<Button>login</Button>
 					<p class="message">
-						Not registered? <a onClick="">Create an account</a>
+						Not registered?{' '}
+						<a onClick="">Create an account</a>
 					</p>
 				</form>
 			</div>
