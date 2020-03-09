2738f56800ff39fc513295567ef7cc4d01c8591e
Jan <jan.andrade@capgemini.com>
2020-03-09 18:42:00 -0300

    utilizando material ui components

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,16 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import {
+	Button,
+	TextField,
+	Link,
+	Typography,
+	Form,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import './styles.css';
 
 const useStyles = makeStyles({
-	padding: '15px',
-	boxSizing: 'border-box',
 	underline: {
 		'&&&:before': {
 			borderBottom: 'none',
@@ -20,36 +24,132 @@ const useStyles = makeStyles({
 export default function login(props) {
 	const [isRegister, setIsRegister] = useState(false);
 	const classes = useStyles();
+
+	function handleChangeInternalPage() {
+		console.log(isRegister);
+		isRegister === false
+			? setIsRegister(true)
+			: setIsRegister(false);
+	}
+
+	function handleCreateAccount(e) {
+		e.preventDefault();
+		console.log('criou conta');
+	}
+
+	function handleLogin(e) {
+		e.preventDefault();
+		console.log('fez login');
+	}
+
+	const submit = e => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className="login-page">
 			<div className="form">
-				<form className="register-form">
-					<TextField
-						label="name"
-						variant="outlined"
-						InputProps={{ classes }}
-					/>
-					<input type="password" placeholder="password" />
-					<input type="text" placeholder="email address" />
-					<Button>create</Button>
-					<p class="message">
-						Already registered? <a href="#">Sign In</a>
-					</p>
-				</form>
-				<form className="login-form">
-					<input type="text" placeholder="username" />
-					<TextField
-						label="name"
-						variant="filled"
-						InputProps={{ classes }}
-					/>
-					<input type="password" placeholder="password" />
-					<Button>login</Button>
-					<p class="message">
-						Not registered?{' '}
-						<a onClick="">Create an account</a>
-					</p>
-				</form>
+				{isRegister === false ? (
+					<form
+						className="login-form"
+						onSubmit={handleLogin}
+					>
+						<TextField
+							fullWidth="true"
+							label="email"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+
+						<TextField
+							fullWidth="true"
+							label="Senha"
+							type="password"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+						<Button type="submit">login</Button>
+						<Typography className={classes.root}>
+							<p class="message">
+								Não é Cadastrado?
+								<Link
+									href="#"
+									onClick={e =>
+										handleChangeInternalPage()
+									}
+								>
+									Realizar Cadastro
+								</Link>
+							</p>
+						</Typography>
+					</form>
+				) : (
+					<form
+						className="login-form"
+						onSubmit={handleCreateAccount}
+					>
+						<TextField
+							fullWidth="true"
+							label="Nome Completo"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+
+						<TextField
+							fullWidth="true"
+							label="email"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+
+						<TextField
+							fullWidth="true"
+							label="Senha"
+							type="password"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+						<TextField
+							fullWidth="true"
+							label="Confirmação de Senha"
+							type="password"
+							variant="filled"
+							InputProps={{ classes }}
+							style={{
+								marginBottom: 5,
+							}}
+						/>
+						<Button type="submit">Criar Conta</Button>
+						<Typography className={classes.root}>
+							<p class="message">
+								Já Cadastrado?
+								<Link
+									href="#"
+									onClick={e =>
+										handleChangeInternalPage()
+									}
+								>
+									Fazer Login
+								</Link>
+							</p>
+						</Typography>
+					</form>
+				)}
 			</div>
 		</div>
 	);
